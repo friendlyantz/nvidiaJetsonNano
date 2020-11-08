3dc72bfb76ff04034a8b90eb26f7504d1c375ece
Anton <panteleev23@gmail.com>
2020-11-09 07:33:15 +1100

    backup

--- openCV/openCV1.py
+++ openCV/openCV1.py
@@ -21,30 +21,39 @@ flip=0
 camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 
 # ==VIDEO CAPTURE MODES -FILE or -CAMERA
-# # =FROM CAMERA
+videoPathRead = '/home/anton/Videos/nvidiaJetsonStream/jetsonStream2.avi'
+# =FROM CAMERA
 cam=cv2.VideoCapture(camSet)
 # =FROM FILE
-# cam=cv2.VideoCapture()
+# cam=cv2.VideoCapture(videoPathRead)
+
 
 # ====> WRITE VIDEO TO FILE SETTINGS
 # =FILEPATH
-videoPath = '/home/anton/Videos/nvidiaJetsonStream/jetsonStream.avi'
-# =WRITING
-outVid=cv2.VideoWriter(videoPath,cv2.VideoWriter_fourcc(*'XVID'),21,(dispW,dispH))
+# =WRITING - UNCOMMENT THIS OR CHANGE VIDEOPATH WHEN READING FROM FILE - also ENABLE WRITING IN WINDOW 1
+videoPathWrite = '/home/anton/Videos/nvidiaJetsonStream/jetsonStream.avi'
+outVid=cv2.VideoWriter(videoPathWrite,cv2.VideoWriter_fourcc(*'XVID'),21,(dispW,dispH))
 
 # ~~~~~~~~~~~MAIN SEQUENCE~~~~~~~~~~~~
 while True:
     ret, frame=cam.read()
     # ===========God's particle===========
 
+    # rectangual-circle frames,corner,corner/radius, BGR, thickness(-1 for fill shape)
+    frame=cv2.rectangle(frame,(340,100),(400,170),(255,0,0),6)
+    frame=cv2.circle(frame,(340,240),50,(0,0,255),-1)
+    
+    fnt=cv2.o
+
     # ============Prometheus==============
 
-# ===> WRITE VIDEO
-# Window 1 - DISPLAY AND WRITE
+# Window 1 - DISPLAY AND WRITE!!!!!
     cv2.imshow('nanoCam',frame)
     # window position
     cv2.moveWindow('nanoCam',0,0)
+# ===> WRITE VIDEO
     outVid.write(frame)
+
 # # Window 2
 #     cv2.imshow('nanoCam2',frame)
 #     cv2.moveWindow('nanoCam2',705,0)
@@ -58,9 +67,11 @@ while True:
 # # Window 4 -RESIZED - S
 #     cv2.imshow('grayVideo2',graySmall)
 #     cv2.moveWindow('grayVideo2',705,520)
-    # exit algorithm
+
+    # exit algorithm (WAITKEY: 1msec for no delay OR LAGGY 50sec wait to match 21 framerate roughly)
     if cv2.waitKey(1)==ord('q'):
         break
+
 # CLEARING MEMORY
 cam.release()
 outVid.release()
